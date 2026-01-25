11f42d0aa0938f8894f0c0dedaed1076a8c6b63d
Nicolas Balmer <nicolas_balmer@hotmail.com>
2026-01-25 16:12:54 +0100

    Exclude sw.js from middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -13,8 +13,9 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - manifest.webmanifest (PWA manifest)
+     * - sw.js (Service Worker)
      * - public folder
      */
-    '/((?!_next/static|_next/image|favicon.ico|manifest\\.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest\\.webmanifest|sw\\.js|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
