aba6752a7c64ee7d81130870f2902c74655eefca
Nicolas Balmer <nicolas_balmer@hotmail.com>
2026-01-25 17:50:47 +0100

    style improvements for total fuel refilled

--- app/components/vehicles.tsx
+++ app/components/vehicles.tsx
@@ -384,7 +384,7 @@ const FlottenUebersicht: FC = () => {
               strokeWidth="1.5"
               viewBox="0 0 24 24"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h6v13H3V3zm0 0h6m-6 0V3m6 13h3.5a2 2 0 002-2V9.5m0 0l2.5-2.5M18 7l-2.5 2.5M18 7v7a2 2 0 01-2 2m0 0H9m9 4.5h-.01M3 21h6" />
             </svg>
           </div>
         </div>
