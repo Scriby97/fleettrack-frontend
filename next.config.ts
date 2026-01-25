50869d01ad21bfe8edbdbfb22752379fafc874b5
Nicolas Balmer <nicolas_balmer@hotmail.com>
2026-01-25 15:44:38 +0100

    Add headers config for manifest.json in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
